151f8603d3aae0650446dd5f55031c360513ca6d
Sam Wilson <samwilsonme@gmail.com>
2025-09-28 18:37:18 +0100

    seo and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bamboosky.co.uk',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('404') && !page.includes('thank-you'),
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
+  ]
 });
